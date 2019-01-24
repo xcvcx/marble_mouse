dbc699241a11e6f1ac9d8ed9641ff7ef6cedd2dc
Sebastian Szkoda <sebastian.szkoda@gmail.com>
2019-01-24 16:40:26 +0100

    Added the script

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # marble_mouse
+
+Script to set-up Logitech Marble Mouse on Ubuntu systems.
+
+usage:
+    . marble.sh
+
+and you are ready to go.
+
+The script does the following mapping:
+    - to scroll hold left small button
+    - to go-back single click left small button
---
+++ marble.sh
@@ -0,0 +1,9 @@
+xinput set-button-map "Logitech USB Trackball" 1 2 3 4 5 6 7 8 9
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 8 8
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation Axes" 8 6 7 4 5
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation X Axis" 8 6
+xinput set-int-prop "Logitech USB Trackball" "Evdev Drag Lock Buttons" 8 9
+xinput --set-prop "Logitech USB Trackball" "libinput Scroll Method Enabled" 0 0 1
+xinput --set-prop "Logitech USB Trackball" "libinput Button Scrolling Button" 8
+xinput --set-prop "Logitech USB Trackball" "Coordinate Transformation Matrix" 2.000000, 0.000000, 0.000000, 0.000000, 2.000000, 0.000000, 0.000000, 0.000000, 1.000000
